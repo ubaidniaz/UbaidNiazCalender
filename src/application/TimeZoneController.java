c50391271c95203d5ba189777a920df7e51355f0
ubaidullah.niaz <ubaidullah.niaz@IC-CS233-44.uc.ucalgary.ca>
2022-08-15 16:36:31 -0600

    Updated TimeZoneFXML interface and added the FXML tags to TimeZoneController

--- src/application/TimeZoneController.java
+++ src/application/TimeZoneController.java
@@ -1,6 +1,27 @@
 package application;
 
+import java.util.TimeZone;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 
 public class TimeZoneController {
+	
+	@FXML
+	private Label localTimeLabel;
+	
+	@FXML
+	private Label resultTimeLabel;
+	
+	@FXML
+	private ComboBox <String> countriesComboBox;
+	
+	
+	@FXML
+	void getTimeZoneButton (ActionEvent event){
+		
+	}
 
-}
+} 
