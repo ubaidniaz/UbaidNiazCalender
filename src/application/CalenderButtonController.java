87f1433375a4aa59539a5601fb6f22656499f4b1
ubaidullah.niaz <ubaidullah.niaz@IC-CS233-33.uc.ucalgary.ca>
2022-08-14 23:10:25 -0600

    Added some textfields and label to FXML file as rough start.

--- src/application/CalenderButtonController.java
+++ src/application/CalenderButtonController.java
@@ -1,6 +1,27 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.YearMonth;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class CalenderButtonController {
 
+    @FXML
+    private TextField yearTextfield;
+
+    @FXML
+    private TextField monthTextfield;
+    
+    @FXML
+    private Label calenderLabel;
+    
+   @FXML
+   void displayCalender(ActionEvent event) {
+	   
+   }
+//	 
 }
