3bbdf722fce1582d908a8646760dc894be84074c
ubaidullah.niaz <ubaidullah.niaz@IC-CS233-44.uc.ucalgary.ca>
2022-08-15 22:58:03 -0600

    Initialized the TimeControllerClass and added an ObservableArrayList with list of countries

--- src/application/TimeZoneController.java
+++ src/application/TimeZoneController.java
@@ -1,13 +1,19 @@
 package application;
 
-import java.util.TimeZone;
+import java.net.URL;
 
+import java.util.ResourceBundle;
+
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 
-public class TimeZoneController {
+public class TimeZoneController implements Initializable {
 	
 	@FXML
 	private Label localTimeLabel;
@@ -17,11 +23,21 @@ public class TimeZoneController {
 	
 	@FXML
 	private ComboBox <String> countriesComboBox;
+	ObservableList<String> 
+	listOfCountries=FXCollections.observableArrayList("Australia","Algeria","Brazil","China","Denmark","Egypt","France","Greenland","Honduras",
+			"Italy","India","Japan","Kuwait","Mexico","Nigeria","Russia","South Africa","Sweden","Thailand","Yemen","Zimbabwe");
 	
+
 	
 	@FXML
 	void getTimeZoneButton (ActionEvent event){
 		
 	}
 
-} 
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
+
